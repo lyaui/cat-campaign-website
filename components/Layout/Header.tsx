842eb07d0d418129b9384efd993bae101545c445
Iris Huang <lyaui@outloook.com>
2023-11-08 02:31:44 +0800

    feat(Header): import donate button

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -4,11 +4,19 @@ import { useRouter } from 'next/router';
 
 import { ROUTERS } from '@/constants/index';
 import logoDark from '@/public/assets/logos/logo_dark.svg';
+import Button from '@/components/UI/Button';
 import Container from '@/components/Layout/Container';
 
 function Header() {
   const router = useRouter();
   const { asPath } = router;
+
+  const navItems = [
+    ROUTERS.STANCE,
+    ROUTERS.CAMPAIGN,
+    ROUTERS.POLICY,
+    ROUTERS.SERVICE_MAIL,
+  ];
 
   const isActiveStyle = (hash: string) => {
     const baseClasses = 'font-bold py-2';
@@ -18,6 +26,10 @@ function Header() {
       : baseClasses;
   };
 
+  function handleDonateClick() {
+    router.push({ pathname: `/`, hash: ROUTERS.DONATE.hash });
+  }
+
   return (
     <header className='h-[100px] bg-white flex-center'>
       <Container className='flex items-center justify-between'>
@@ -26,8 +38,8 @@ function Header() {
         </Link>
 
         <nav>
-          <ul className='flex gap-x-12'>
-            {Object.values(ROUTERS).map((_route) => (
+          <ul className='flex items-center gap-x-12'>
+            {navItems.map((_route) => (
               <li key={_route.hash} className={isActiveStyle(_route.hash)}>
                 <Link
                   href={`/${_route.hash ? '#' + _route.hash : _route.hash}`}
@@ -37,7 +49,9 @@ function Header() {
                 </Link>
               </li>
             ))}
-            <li>BUTTON</li>
+            <li>
+              <Button onClick={handleDonateClick}>小額捐款</Button>
+            </li>
           </ul>
         </nav>
       </Container>
