0891ee9d8a3992fb69ef47b9677cee319f7cc6ef
Iris Huang <lyaui@outloook.com>
2023-11-08 01:27:01 +0800

    feat(constants): add ROUTERS constant

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const ROUTERS = {
+  STANCE: { name: '候選人主張', hash: '' },
+  CAMPAIGN: { name: '最新活動', hash: 'campaign' },
+  POLICY: { name: '政策議題', hash: 'policy' },
+  SERVICE_MAIL: { name: '民眾服務信箱', hash: 'service-main' },
+};
