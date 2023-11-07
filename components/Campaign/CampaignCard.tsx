a6785432fee9177312a28b218cf86da510807276
Iris Huang <lyaui@outloook.com>
2023-11-08 06:52:24 +0800

    feat(components): add vertical  CampaignCard

---
+++ components/Campaign/CampaignCard.tsx
@@ -0,0 +1,56 @@
+import Image, { type StaticImageData } from 'next/image';
+import dayjs from 'dayjs';
+
+interface CampaignCardProps {
+  direction?: 'column' | 'row';
+  date: string;
+  title: string;
+  desc: string;
+  img: StaticImageData;
+}
+
+function CampaignCard({
+  direction = 'column',
+  date = '',
+  title = '',
+  desc = '',
+  img = '',
+}: CampaignCardProps) {
+  const dateNum = dayjs(date).format('D');
+  const engMonth = dayjs(date).format('MMM') + '.';
+  const formattedDate = dayjs(date).format('YYYY.M.D');
+  if (direction === 'row') {
+  }
+  return (
+    <div className='rounded-lg bg-primary overflow-hidden'>
+      <div className='relative w-full h-[325px] overflow-hidden [&>img]:hover:scale-105 [&>img]:transition'>
+        <div className='absolute z-10 right-0 w-[74px] h-[74px] bg-secondary text-title-light font-bold	column-center'>
+          <span className='text-4xl'>{dateNum}</span>
+          <span className='text-base'>{engMonth}</span>
+        </div>
+        <Image
+          src={img}
+          alt={title}
+          fill={true}
+          draggable={false}
+          objectFit='cover'
+          style={{ position: 'absolute' }}
+        />
+        <div
+          className='absolute z-10 w-0 h-0 bottom-0 left-1/2 -translate-x-1/2'
+          style={{
+            borderStyle: 'solid',
+            borderWidth: '0 22.5px 25px 22.5px',
+            borderColor: 'transparent transparent #ffffff transparent',
+          }}
+        />
+      </div>
+      <div className='p-4 pb-10 bg-white'>
+        <h4 className='heading-4 mb-2.5'>{title}</h4>
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CampaignCard;
--- components/Campaign/index.tsx
+++ components/Campaign/index.tsx
@@ -3,22 +3,29 @@ import Image from 'next/image';
 import { ROUTERS } from '@/constants/index';
 import campaignTitle from '@/public/assets/font-svg/campaign.svg';
 import Container from '@/components/Layout/Container';
+import CampaignCard from '@/components/Campaign/CampaignCard';
+import campaignImg1 from '@/public/assets/images/campaign_1.png';
+import campaignImg2 from '@/public/assets/images/campaign_2.png';
+import campaignImg3 from '@/public/assets/images/campaign_3.png';
 
 const campaigns = [
   {
     date: '2023-12-26',
     title: '參與台北寵物論壇 爭取貓咪友善環境',
     desc: '炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！我相信，我們的都市中，每一隻貓咪都應該有自己的 VIP 休憩空間。',
+    img: campaignImg1,
   },
   {
     date: '2023-12-26',
     title: '帶著你的貓耳，來和我一起走！',
     desc: '街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！這次的活動不僅讓我看到大家的熱情，更加堅定了我推進「貓咪友善環境」政策的決心。',
+    img: campaignImg2,
   },
   {
     date: '2023-12-20',
     title: '收容所模特兒大比拼！',
     desc: '活動三消息內容：今天的收容所不再是一片寂靜。為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！',
+    img: campaignImg3,
   },
 ];
 
@@ -28,7 +35,7 @@ function Campaign() {
   return (
     <section id={currentRouter.hash} className='py-11'>
       <Container>
-        <h2 className='flex-center mb-5'>
+        <h2 className='flex-center mb-8'>
           <Image
             src={campaignTitle}
             alt={currentRouter.name}
@@ -38,9 +45,8 @@ function Campaign() {
           />
         </h2>
         <div className='grid grid-cols-12 grid-rows-2 gap-[30px]'>
-          <div className='bg-primary col-span-5 row-span-2'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
-            distinctio
+          <div className='col-span-5 row-span-2'>
+            <CampaignCard {...campaigns[0]} />
           </div>
           <div className='bg-secondary col-span-7 row-span-1'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
