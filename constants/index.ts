6472536a3456a9a4669865e6321f643036284452
Iris Huang <lyaui@outloook.com>
2023-11-08 02:30:54 +0800

    feat(constants): add DONATE to ROUTERS

--- constants/index.ts
+++ constants/index.ts
@@ -2,5 +2,6 @@ export const ROUTERS = {
   STANCE: { name: '候選人主張', hash: '' },
   CAMPAIGN: { name: '最新活動', hash: 'campaign' },
   POLICY: { name: '政策議題', hash: 'policy' },
+  DONATE: { name: '小額捐款', hash: 'donate' },
   SERVICE_MAIL: { name: '民眾服務信箱', hash: 'service-main' },
 };
