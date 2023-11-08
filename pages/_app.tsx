b4c8cb0df40ab3f0d64a9ff5adfd5ba25b99d33d
Iris Huang <lyaui@outloook.com>
2023-11-08 19:34:01 +0800

    choreI: install Aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+
 import '../styles/globals.css';
 import 'normalize.css/normalize.css';
+import 'aos/dist/aos.css';
 import type { AppProps } from 'next/app';
 
 import Header from '@/components/Layout/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      easing: 'ease-out-cubic',
+      once: false,
+      offset: 50,
+    });
+  }, []);
+
+  useEffect(() => {
+    AOS.refresh();
+  }, []);
   return (
     <main className='h-full bg-background border-x-[5px] border-primary'>
       <Header />
