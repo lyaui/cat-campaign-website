35965a6f15c8ac8bbc79df73a429e7b928c8e542
Iris Huang <lyaui@outloook.com>
2023-11-08 23:31:53 +0800

    fix(constants): modify service mail

--- constants/index.ts
+++ constants/index.ts
@@ -3,7 +3,7 @@ export const ROUTERS = {
   CAMPAIGN: { name: '最新活動', hash: 'campaign' },
   POLICY: { name: '政策議題', hash: 'policy' },
   DONATE: { name: '小額捐款', hash: 'donate' },
-  SERVICE_MAIL: { name: '民眾服務信箱', hash: 'service-main' },
+  CONTACT_US: { name: '民眾服務信箱', hash: 'contact-us' },
 };
 
 export const DONATE_PANEL = {
