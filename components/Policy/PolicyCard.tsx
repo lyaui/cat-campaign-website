80bf68daac71ffe24d545cef4f128890e0107757
Iris Huang <lyaui@outloook.com>
2023-11-09 22:07:28 +0800

    feat(style): PolicyCard hover effect

--- components/Policy/PolicyCard.tsx
+++ components/Policy/PolicyCard.tsx
@@ -8,17 +8,21 @@ interface PolicyCardProps {
 
 function PolicyCard({ title, order, policies }: PolicyCardProps) {
   return (
-    <div className='flex flex-col gap-[10px] bg-white p-4 rounded-lg flex-1'>
-      <Badge>{`政策${order}`}</Badge>
-      <h4 className='heading-4'>{title}</h4>
-      <ol>
-        {policies.map((_policy, index) => (
-          <li key={index} className='flex mb-2'>
-            <p className='w-[24px] shrink-0'>{`${index + 1}.`}</p>
-            <p>{_policy}</p>
-          </li>
-        ))}
-      </ol>
+    <div className='group flex flex-col gap-[10px] bg-white p-4 rounded-lg flex-1 overflow-hidden'>
+      <Badge
+        className={`group-hover:-translate-y-10 group-hover:opacity-0 c-transition`}
+      >{`政策${order}`}</Badge>
+      <div className={`group-hover:-translate-y-10 c-transition`}>
+        <h4 className='heading-4 mb-2.5'>{title}</h4>
+        <ol>
+          {policies.map((_policy, index) => (
+            <li key={index} className='flex mb-2'>
+              <p className='w-[24px] shrink-0'>{`${index + 1}.`}</p>
+              <p>{_policy}</p>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
