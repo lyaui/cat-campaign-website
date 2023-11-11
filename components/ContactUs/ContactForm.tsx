fbf3b3cac97bab0fa915b84e1b5ebb5660f9fe64
Iris Huang <lyaui@outloook.com>
2023-11-12 07:14:35 +0800

    feat(Input): add text area

--- components/ContactUs/ContactForm.tsx
+++ components/ContactUs/ContactForm.tsx
@@ -6,12 +6,16 @@ function ContactForm() {
     { id: 'name', placeholder: '您的姓名' },
     { id: 'email', placeholder: '您的 Email' },
     { id: 'mobile', placeholder: '您的手機' },
-    { id: 'comment', placeholder: '您的建言' },
+    { id: 'comment', placeholder: '您的建言', multiline: true },
   ];
   return (
     <form className='column-center w-full gap-5 mt-5'>
       {inputs.map((_input) => (
-        <Input key={_input.id} placeholder={_input.placeholder} />
+        <Input
+          key={_input.id}
+          placeholder={_input.placeholder}
+          multiline={_input.multiline || false}
+        />
       ))}
 
       <Button size='large'>送出意見</Button>
--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -1,17 +1,39 @@
-import { useImperativeHandle, type ComponentPropsWithoutRef } from 'react';
+import { type ComponentPropsWithoutRef } from 'react';
 
-type InputProps = ComponentPropsWithoutRef<'input'> & {
-  error?: false;
-};
+interface InputProps extends ComponentPropsWithoutRef<'input'> {
+  error?: boolean;
+  multiline?: never;
+}
 
-function Input(props: InputProps) {
-  const { error = false, ...others } = props;
+interface TextAreaProps extends ComponentPropsWithoutRef<'textarea'> {
+  error?: boolean;
+  multiline: boolean;
+}
+
+function isTextAreaProps(
+  props: InputProps | TextAreaProps,
+): props is TextAreaProps {
+  return props.multiline === true;
+}
+
+function Input(props: InputProps | TextAreaProps) {
+  if (isTextAreaProps(props)) {
+    return (
+      <textarea
+        className={`h-[100px] w-full px-3 py-2.5 rounded-lg body-normal border ${
+          props.error ? 'border-error' : 'border-gray-300'
+        } placeholder:body-normal placeholder:text-gray-100`}
+        {...props}
+      />
+    );
+  }
+
   return (
     <input
       className={`h-[50px] w-full px-3 py-2.5 rounded-lg body-normal border ${
-        error ? 'border-error' : 'border-gray-300'
+        props.error ? 'border-error' : 'border-gray-300'
       } placeholder:body-normal placeholder:text-gray-100`}
-      {...others}
+      {...props}
     />
   );
 }
