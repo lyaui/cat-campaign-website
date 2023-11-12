a8c4ad5bf5a9e66b72d3e4ec8a4b9a1b739d5807
Iris Huang <lyaui@outloook.com>
2023-11-12 15:21:41 +0800

    feat: add header info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import { useEffect } from 'react';
+import Head from 'next/head';
 import AOS from 'aos';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import 'animate.css';
-
-import '../styles/globals.css';
 import 'normalize.css/normalize.css';
 import 'aos/dist/aos.css';
 import type { AppProps } from 'next/app';
 
+import '../styles/globals.css';
 import Header from '@/components/Layout/Header';
 import Footer from '@/components/Layout/Footer';
+
+const header = {
+  title: '喵立翰 Miao Li-Han | 2023 從喵的眼中看見台灣',
+  content: '2023 THE F2E 立委競選，護航台灣幸福經濟 從照顧每一隻貓咪開始',
+  image: 'https://i.imgur.com/wYtejfg.png',
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -28,6 +34,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ParallaxProvider>
+      <Head>
+        <title>{header.title}</title>
+        <meta name='description' content={header.content} />
+        <meta property='og:title' content={header.title} />
+        <meta property='og:image' content={header.image} />
+        <meta property='og:description' content={header.content} />
+      </Head>
       <div className='h-full bg-background border-x-[5px] border-primary'>
         <Header />
         <main className={desktopMT + mobileMt}>
